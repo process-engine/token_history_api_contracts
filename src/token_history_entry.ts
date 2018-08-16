8bcf1137757a37bc6951ebebc17eeb0b4be353b3
ElRaptorus <christian.werner@5minds.de>
2018-08-16 14:48:17 +0200

    Add review notes

--- src/token_history_entry.ts
+++ src/token_history_entry.ts
@@ -1,5 +1,13 @@
-import {Runtime} from '@process-engine/process_engine_contracts';
+import {IIdentity} from '@essential-projects/iam_contracts';
 
-export class TokenHistoryEntry extends Runtime.Types.ProcessToken {
-
+export class TokenHistoryEntry {
+  // header
+  public processInstanceId: string;
+  public processModelId: string;
+  public correlationId: string;
+  public identity: IIdentity;
+  public createdAt: Date;
+  public caller: string; // empty if this started the correlation
+  // payload
+  public payload: any;
 }
