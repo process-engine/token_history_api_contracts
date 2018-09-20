2f9a5fb6eb518471ae98ef4317577e7056188718
ElRaptorus <christian.werner@5minds.de>
2018-09-20 12:34:15 +0200

    :fire: Remove Service suffix from ITokenHistoryApi

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './itoken_history_api_service';
+export * from './itoken_history_api';
 export * from './rest_settings';
 export * from './token_history_entry';
 export * from './token_event_type';
