138c59edefdd19e095180399808a479425d4c8a0
Steffen Knaup <steffen.knaup@5minds.de>
2018-12-06 12:50:41 +0100

    :pencil2: Fix Typo

--- src/token_history_group.ts
+++ src/token_history_group.ts
@@ -3,4 +3,4 @@ import { TokenHistoryEntry } from './token_history_entry';
 /**
  * Describes a Group of Process Token of a specific Process Instance.
  */
-export type TokenHistoryGroup = Map<string, Array<TokenHistoryEntry>;
+export type TokenHistoryGroup = Map<string, Array<TokenHistoryEntry>>;
