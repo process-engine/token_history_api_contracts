dfedc336fb9491ef6f0057766a5f6f655b50abcd
Steffen <Steffen.knaup@5minds.de>
2018-12-06 12:37:57 +0100

    :lipstick: Add Type For TokenHistoryGroup

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './itoken_history_api';
 export * from './rest_settings';
 export * from './token_history_entry';
 export * from './token_event_type';
+export * from './token_history_group';
---
+++ src/token_history_group.ts
@@ -0,0 +1,6 @@
+import { TokenHistoryEntry } from './token_history_entry';
+
+/**
+ * Describes a Group of Process Token of a specific Process Instance.
+ */
+export type TokenHistoryGroup = Map<string, Array<TokenHistoryEntry>;
