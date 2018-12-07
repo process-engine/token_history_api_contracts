0fb1fd1ec4f052473ed34a5e91767758876cc588
ElRaptorus <christian.werner@5minds.de>
2018-12-07 09:47:35 +0100

    :recycle: Refactor TokenHistoryGroup type into dictionary

--- src/token_history_group.ts
+++ src/token_history_group.ts
@@ -3,4 +3,4 @@ import {TokenHistoryEntry} from './token_history_entry';
 /**
  * Describes a group of process token of a specific process instance.
  */
-export type TokenHistoryGroup = Map<string, Array<TokenHistoryEntry>>;
+export type TokenHistoryGroup = {[processInstanceId: string]: Array<TokenHistoryEntry>};
